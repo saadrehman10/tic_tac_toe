5913c4a4597fc7b4d7236b09f20b6d66a183ac6c
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-06-06 11:37:17 +0500

    update

--- lib/tic_tac_toe.dart
+++ lib/tic_tac_toe.dart
@@ -8,20 +8,41 @@ class TicTacToe extends StatefulWidget {
 }
 
 class _TicTacToeState extends State<TicTacToe> {
+  List<bool> tilesState = List.generate(9, (index) => true);
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: const Text('T I C  T A C  T O E', style: TextStyle(
-          color: Colors.black,
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
+        title: const Text(
+          'T I C  T A C  T O E',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         centerTitle: true,
-        
-
       ),
+      body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text('Player "X"'),
+            SizedBox(
+              width: double.infinity,
+              height: 400,
+              child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3),
+                  itemCount: 9,
+                  itemBuilder: (BuildContext context, index) {
+                    return MaterialButton(
+                      onPressed: () {},
+                      // child: ,
+                    );
+                  }),
+            )
+          ]),
     );
   }
 }
