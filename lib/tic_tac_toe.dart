913fed82a7250cdf6d4d24ea41cad329fa62dcfb
Saad ur Rehmah <125679304+saadrehman10@users.noreply.github.com>
2024-06-06 10:42:05 +0500

    update

--- lib/tic_tac_toe.dart
+++ lib/tic_tac_toe.dart
@@ -1,1 +1,27 @@
-stl
+import 'package:flutter/material.dart';
+
+class TicTacToe extends StatefulWidget {
+  const TicTacToe({super.key});
+
+  @override
+  State<TicTacToe> createState() => _TicTacToeState();
+}
+
+class _TicTacToeState extends State<TicTacToe> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('T I C  T A C  T O E', style: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+        ),
+        centerTitle: true,
+        
+
+      ),
+    );
+  }
+}
